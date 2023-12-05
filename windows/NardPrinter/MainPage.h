9890275941b6df9af3497c3de2f25825b4937c44
MHF0 <moh.hesham.f@gmail.com>
2023-12-06 00:34:22 +0300

    Add Windows

---
+++ windows/NardPrinter/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::NardPrinter::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::NardPrinter::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
